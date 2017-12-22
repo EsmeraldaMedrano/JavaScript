f7ae3b092adcaab16846ceb85dc5c64130d5fd7f
Esmeralda Medrano <esmeralda.medrano@cceo.com.mx>
2017-12-22 16:44:58 -0600

    Added a exercice about promises

--- javascript-node/index.js
+++ javascript-node/index.js
@@ -2,7 +2,7 @@
 request('http://google.com', function(){
   console.log('Terminé la petición de google');
 });
-console.log('Yo voy despues de la peticion a google');*/
+console.log('Yo voy despues de la peticion a google')
 
 //Promesa  Estados; fullfield:completa, rejected:no terminada con exito(error)
 //pending:pendiente, setled: finalizada con exito o con error
@@ -12,4 +12,23 @@ rp('http://google.com') // devuelve un objeto tipo Promise;
       console.log('Terminé la petición de google');
   }).catch(function(err){
     console.log(err)
+  });*/
+const request = require('request');
+
+function leer(url){
+  return new Promise(function(resolve, reject){
+    request(url, function(err, response){
+      if(err) {
+        reject(err);
+      }else{
+        resolve(response);
+      }
+    });
   });
+}
+
+leer('http://codigofacilito.com').then(function(response){
+  console.log(response);
+}).catch  (function(err){
+  console.log(err);
+});
