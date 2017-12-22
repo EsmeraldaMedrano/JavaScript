f0cabd66958c108efd1648395f9b6cdc9024b5b6
Esmeralda Medrano <esmeralda.medrano@cceo.com.mx>
2017-12-22 17:07:29 -0600

    Added comments about multiple promises

--- javascript-node/index.js
+++ javascript-node/index.js
@@ -12,7 +12,7 @@ rp('http://google.com') // devuelve un objeto tipo Promise;
       console.log('Terminé la petición de google');
   }).catch(function(err){
     console.log(err)
-  });*/
+  });
 const request = require('request');
 
 function leer(url){
@@ -31,4 +31,20 @@ leer('http://codigofacilito.com').then(function(response){
   console.log(response);
 }).catch  (function(err){
   console.log(err);
-});
+});*/
+
+//multimples promesas
+
+let p1 = new Promise((resolve, reject)=> setTimeout(resolve,500,'HolaMundo'));
+let p2 = new Promise((resolve, reject)=> setTimeout(resolve,600,'segundo HolaMundo'));
+let p3 = Promise.reject();
+let saluda = ()=> console.log("Hola a todos");
+/*p1.then(function(){
+  p2.then(function(){
+    saluda();
+  });
+});*/ //infierno del callback
+
+Promise.all([p1,p2,p3]).then(resultados=>{ //genera una promesa en caso de que sea exitosa o falle
+  saluda();
+}).catch(()=> console.log('fallé'));
